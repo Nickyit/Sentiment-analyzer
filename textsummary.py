9a3a6c54ac58597254dc596820c749f6eb05551b
Nikita Digodiya <nikitadigodiya31@gmail.com>
2026-01-08 19:22:07 +0530

    Creating an basic User Interface(UI) for my Application

--- textsummary.py
+++ textsummary.py
@@ -10,18 +10,31 @@ summarizer = pipeline(
     dtype=torch.float32
 )
 
-text = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and 
-        entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has
-         been the wealthiest person in the world since 2021; as of December 2025, Forbes
-        estimates his net worth to be around US$717 billion..."""
+# text = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and 
+#         entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has
+#          been the wealthiest person in the world since 2021; as of December 2025, Forbes
+#         estimates his net worth to be around US$717 billion..."""
 
-result = summarizer(
-    text,
-    max_new_tokens=150,
-    truncation=True,
-    do_sample=False
-)
+# result = summarizer(
+#     text,
+#     max_new_tokens=150,
+#     truncation=True,
+#     do_sample=False
+# )
 
-print(result[0]["summary_text"])
+# print(result[0]["summary_text"])
 # print(summarizer.model.config)
 # print(summarizer(text, max_new_tokens=100, truncation=True, do_sample=False))
+
+# importing gradio to close any existing interfaces
+import gradio as gr
+
+gr.close_all()
+# We create a function bcs gradio needs a function to work with
+
+def summary(input):
+    output = summarizer(input)
+    return output[0]["summary_text"]
+
+interfacee = gr.Interface(fn=summary, inputs="text", outputs="text")
+interfacee.launch()
